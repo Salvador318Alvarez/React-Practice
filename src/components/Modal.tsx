7d886516ebce90bb81aecac520d8d1c48bf0a78f
Salvador Alvarez <111669165+Salvador318Alvarez@users.noreply.github.com>
2023-01-16 11:20:36 -0600

    learned about portals

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,13 @@
+import ReactDOM  from "react-dom"
+
 export default function Modal({children, handleClose}) { //or props with props.children down there
-    return (
+    return ReactDOM.createPortal(( //to move it's place in the html
       <div className="modal-backdrop">
            <div className="modal"> 
               {children}  
               <button onClick={handleClose}>close</button>
           </div>     
-   
           {/* handle close is defined in the parent component */}
       </div>
-    )
+    ), document.body /*moves it to the bottom of the body tag */)
   }
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Modal from "../components/Modal";
 
 const History = () => {
+    const [showModal, setShowModal] = useState(false)
+
+    const handleClose = () => {
+        setShowModal(false)
+    }
+
+    const handleOpen = () => {
+        setShowModal(true)
+    }
+    //you can pass in functions as props to affect state!!
+    console.log(showModal);
+    
+
     return ( 
         <section>
-            <h1>History Page</h1>
-            <Modal>
-                <h2>History Page is under contruction</h2>
-                <Link to='/'>Back to Home Page</Link>
-            </Modal>
+       
+            <h1>History</h1>
+            {showModal! && 
+                <Modal handleClose={handleClose}>
+                    <h2>History Page is under construction</h2>
+                    <Link to='/'>Back to Home Page</Link>
+                </Modal> 
+            }
+            
+            <button onClick={handleOpen}>Open Modal</button>
+            
         </section>
      );
 }
