afa167e9cb237aabb5cdbfe938f21524ca4eb052
Salvador Alvarez <111669165+Salvador318Alvarez@users.noreply.github.com>
2023-01-23 11:35:04 -0600

    practice

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,36 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
     const [random, setRandom] = useState([])
+    const [url, setUrl] = useState('https://jsonplaceholder.typicode.com/todos/')
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts/')
+        fetch(url)
         .then(response => response.json())
         .then(json => setRandom(json))
-    }, [])
+    }, [url]) //dependency 
  
     console.log(random);
     
     
     return ( 
         <>
+            <button style={{marginTop: '20vh'}} onClick={() => 
+                (url === 'https://jsonplaceholder.typicode.com/todos/')?
+                setUrl('https://jsonplaceholder.typicode.com/posts/'):
+                setUrl('https://jsonplaceholder.typicode.com/todos/')
+            }
+            >update URL</button>
+            <button style={{marginTop: '20vh'}} onClick={() => 
+                setUrl('https://jsonplaceholder.typicode.com/todos/?completed=true')
+            }
+            >
+                update endpoint
+            </button>
             {random.map(post => (
                 <div key={post.id}>
                     <h3>{post.title}</h3>
                     <p>{post.body}</p>
+                    <p>{post.completed}</p>
                 </div>
             ))}   
         </>
